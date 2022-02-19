c281c15bee37908036446f6b87e0003e96fe8da8
Ананьев Сергей <9852407799@mail.ru>
2022-02-19 04:08:02 +0300

    добавил функцию макс длины строки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const getRandomNum = function(from, to) {
   if (from < to) {
     return Math.abs(Math.floor(Math.random() * (to - from + 1) + from));
@@ -9,4 +10,17 @@ const getRandomNum = function(from, to) {
     return to;
   }
 };
+const getMaxStringLength = function(string, length) {
+  return string.length <= length;
+};
+// Проверка работы функции
+const comment = 'Это тестовый комментарий, не обращайте внимания. Повторяю! Это тестовый комментарий';
+const commentLength = 200;
+if (getMaxStringLength(comment, commentLength)) {
+  console.log('Отличный комментарий!');
+}
+else {
+  console.log('Нужно меньше слов и больше дела!');
+}
+
 getRandomNum(10, 8);
